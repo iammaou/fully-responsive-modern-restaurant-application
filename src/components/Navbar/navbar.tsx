5c717e6df7d3b9b9b27d27daaf3cfe10dcca0193
iammaou <markokubica625@gmail.com>
2023-08-10 12:05:55 +0100

    navbar done

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -3,8 +3,22 @@ import React from 'react';
 import './navbar.css';
 
 const navbar = () => (
-  <div>
-    navbar
+  <div className='restaurant__navbar'>
+    <div>
+      <h1>Gerícht</h1>
+    </div>
+    <div className='restaurant__navbar-navigation'>
+      <a href='#home'>Home</a>
+      <a href='#pages'>Pages</a>
+      <a href='#contact'>Contact</a>
+      <a href='#blog'>Blog</a>
+      <a href='#landing'>Landing</a>
+    </div>
+    <div className='restaurant__navbar-registration'>
+      <a href='#home'>Log in / Registration</a>
+      <div className='restaurant__navbar-registration_line'></div>
+      <a href='#home'>Book Table</a>
+    </div>
   </div>
 );
 
