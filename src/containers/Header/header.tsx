aaf9050a10639d9f5287a5af0fb03604a9af9024
iammaou <markokubica625@gmail.com>
2023-08-14 12:14:47 +0100

    header done + fixes on the navbar

--- src/containers/Header/header.tsx
+++ src/containers/Header/header.tsx
@@ -2,10 +2,19 @@ import React from 'react';
 
 import './header.css';
 
-const header = () => (
-  <div>
-    header
+const Header = () => (
+  <div className='restaurant__header'>
+    <div className='restaurant__header-content'>
+      <h3>Chase the new Flavour</h3>
+      <img src='./assets/spoon.png' alt='spoon'/>
+      <h1 className='golden__text'>The key to Fine dining</h1>
+      <p>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus</p>
+      <button>Explore Menu</button>
+    </div>
+    <div className='restaurant__header-img'>
+      <img src='./assets/welcome.png' alt='welcome'/>
+    </div>
   </div>
 );
 
-export default header;
+export default Header;
