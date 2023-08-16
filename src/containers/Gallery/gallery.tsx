f1e062767d09fe0ca452dfb0fed00709b8efcf8d
iammaou <markokubica625@gmail.com>
2023-08-16 15:12:25 +0100

    gallery done

--- src/containers/Gallery/gallery.tsx
+++ src/containers/Gallery/gallery.tsx
@@ -2,10 +2,21 @@ import React from 'react';
 
 import './gallery.css';
 
-const gallery = () => (
-  <div>
-    gallery
+const Gallery = () => (
+  <div className='app__gallery'>
+    <div className='app__gallery-content'>
+        <h5>Instagram</h5>
+        <img src='./assets/spoon.png' alt='spoon'/>
+        <h2>Photo Gallery</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mu.</p>
+        <button>view more</button>
+    </div>
+    <div className='app__gallery-images'>
+      <img src='./assets/gallery01.png' alt='gallery01'/>
+      <img src='./assets/gallery02.png' alt='gallery02'/>
+      <img src='./assets/gallery03.png' alt='gallery03'/>
+    </div>
   </div>
 );
 
-export default gallery;
+export default Gallery;
