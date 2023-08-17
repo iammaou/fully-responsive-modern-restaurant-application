95770ca682788dfcfc276a8d14889e65695d61e9
iammaou <markokubica625@gmail.com>
2023-08-17 13:01:21 +0100

    news section done

--- src/containers/News/news.tsx
+++ src/containers/News/news.tsx
@@ -3,8 +3,15 @@ import React from 'react';
 import './news.css';
 
 const news = () => (
-  <div>
-    news
+  <div className='app__news'>
+    <h5>Newsletter</h5>
+    <img src='./assets/spoon.png' alt='spoon'/>
+    <h2>Subscribe to Our Newsletter</h2>
+    <p>And never miss latest Updates!</p>
+    <div className='app__news-input'>
+      <input type='email' placeholder='Email Address'/>
+      <button>Subscribe</button>
+    </div>
   </div>
 );
 
